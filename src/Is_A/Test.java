1be9d27a58a1190ffdfa5628aea55a85a4df2779
subhrajit-github <subudhisubhrajit@gmail.com>
2025-08-11 21:45:27 +0530

    commit

---
+++ src/Is_A/Test.java
@@ -0,0 +1,17 @@
+package Is_A;
+class A{
+	int i=10;
+}
+class B extends A{
+	int j=20;
+}
+public class Test {
+     public static void main(String [] args) {
+    	 A a=new A();
+    	 System.out.println(a.i);
+    	// System.out.println(a.j);//Compile time Error
+    	 B b=new B();
+    	 System.out.println(b.i);
+    	 System.out.println(b.j);
+     }
+}
