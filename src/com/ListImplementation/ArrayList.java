317c9411a260f2dc8e14e310f2e1ec66c54e3146
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-09 23:12:05 +0530

    commit

--- src/com/ListImplementation/ArrayList.java
+++ src/com/ListImplementation/ArrayList.java
@@ -1,52 +1,63 @@
 package com.ListImplementation;
+public class ArrayList<E> {
+    private Object[] a = new Object[5];
+    private int p = 0;
 
-public class ArrayList {
-    private Object[] a=new Object[5];
-    private int p=0;
-    public void add(Object e) {
-    	if(p>=a.length) increaseArraySize();
-    	a[p++]=e;
+    public void add(E e) {
+        if (p >= a.length) increaseArraySize();
+        a[p++] = e;
     }
+
     private void increaseArraySize() {
-    	Object[] temp=new Object[a.length+3];
-    	for(int i=0;i<a.length;i++) {
-    		temp[i]=a[i];
-    	}
-    	a=temp;
+        Object[] temp = new Object[a.length + 3];
+        for (int i = 0; i < a.length; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
     }
-    public void add(int index,Object e) {
-    	if(p>=a.length) increaseArraySize();
-    	for(int i=size()-1;i>=index;i--) {
-    		a[i+1]=a[i];
-    	}
-    	a[index]=e;
-    	p++;
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (p >= a.length) increaseArraySize();
+        for (int i = size() - 1; i >= index; i--) {
+            a[i + 1] = a[i];
+        }
+        a[index] = e;
+        p++;
     }
+
     public int size() {
-    	return p;
+        return p;
     }
-    public Object get(int index) {
-    	if(index<=-1 || index>=size()) {
-    		throw new IndexOutOfBoundsException();
-    	}
-    	return a[index];
+
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) a[index];
     }
+
     public void remove(int index) {
-    	if(index<=-1 || index>=size()) {
-    		throw new IndexOutOfBoundsException();
-    	}
-    	for(int i=index+1;i<size();i++) {
-    		a[i-1]=a[i];
-    	}
-    	a[--p]=null;
+        if (index <=-1 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = index + 1; i < size(); i++) {
+            a[i - 1] = a[i];
+        }
+        a[--p] = null;
     }
+
+    @Override
     public String toString() {
-    	if(size()==0) return "[]";
-    	String s="["+a[0];
-    	for(int i=1;i<size();i++) {
-    		s=s+","+a[i];
-    	}
-    	s=a+"]";
-    	return s;
+        if (size() == 0) return "[]";
+        String s="["+a[0];
+        for (int i = 1; i < size(); i++) {
+            s=s+","+a[i];
+        }
+        s=a+"]";
+        return s;
     }
 }
---
+++ src/com/ListImplementation/Test1.java
@@ -0,0 +1,16 @@
+package com.ListImplementation;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("java");
+		l.add("python");
+		
+		String s1=l.get(0);
+		System.out.println(s1);
+		String s2= l.get(1);
+		System.out.println(s2);
+	}
+
+}
