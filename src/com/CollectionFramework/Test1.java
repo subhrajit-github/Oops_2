1fdfff92f3f93803a8185a253f0f856fb0c3bd9b
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-09 23:24:34 +0530

    commit

---
+++ src/com/CollectionFramework/Test1.java
@@ -0,0 +1,24 @@
+package com.CollectionFramework;
+
+import java.util.ArrayList;
+
+public class Test1 {
+     public static void main(String [] args) {
+    	 ArrayList l1=new ArrayList();
+    	 l1.add(10);
+    	 l1.add(20);
+    	 l1.add(30);
+    	 l1.add(40);
+    	 l1.add(50);
+    	 System.out.println("l1 ="+l1);
+    	 
+    	 ArrayList l2=new ArrayList();
+    	 l2.add(30);
+    	 l2.add(50);
+    	 l2.add(60);
+    	 System.out.println("l2 ="+l2);
+    	 
+    	boolean res= l1.containsAll(l2);
+    	 System.out.println("Does l1 contains all the elements of l2? "+res);
+     }
+}
