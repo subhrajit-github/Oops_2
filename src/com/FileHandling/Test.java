05bb1706d8b5a51bd548a191db41699a42587c70
subhrajit-github <subudhisubhrajit@gmail.com>
2025-10-29 22:02:29 +0530

    commit

---
+++ src/com/FileHandling/Test.java
@@ -0,0 +1,21 @@
+package com.FileHandling;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		File f=new File("C:\\Users\\HP\\OneDrive\\FileHandling\\A.text");
+		//System.out.println(f.mkdir());//Folder is created
+		System.out.println(f.createNewFile());
+		System.out.println(f.canExecute());
+		System.out.println(f.canRead());
+		System.out.println(f.canWrite());
+		System.out.println(f.getAbsolutePath());
+		
+		File f1=new File("C:\\Users\\HP\\OneDrive\\FileHandling\\B.text");
+		System.out.println(f.renameTo(f1));
+	}
+
+}
